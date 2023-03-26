cae4e80a2bbd77a43db9fd2caaf29ebe2a147ea0
Süleyman Aslan <suleymanaslan.8006@gmail.com>
2023-03-26 22:34:27 +0300

    Mongodb setup, models & hashing password, resister api, middlewares, react setup

--- config/db.js
+++ config/db.js
@@ -2,7 +2,11 @@ import mongoose from 'mongoose';
 import colors from 'colors';
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL);
+    const conn = await mongoose.connect(process.env.MONGO_URL, {
+      dbName: 'ecommerce',
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(`Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white);
   } catch (error) {
     console.log(`Errro in Mongodb ${error}`.bgRed.white);
