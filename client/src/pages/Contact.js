8689f846dcc4cae0f138cbb83fa4dd04b21a7f3d
Süleyman Aslan <suleymanaslan.8006@gmail.com>
2023-03-27 11:29:59 +0300

    Nav menu

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -10,17 +10,15 @@ const Contact = () => {
         </div>
         <div className="col-md-4">
           <h1 className="bg-dark p-2 text-white text-center">CONTACT US</h1>
-          <p className="text-justify mt-2">
-            any query and info about prodduct feel free to call anytime we 24X7 vaialible
-          </p>
+          <p className="text-justify mt-2">any query and info about prodduct feel free to call anytime we</p>
           <p className="mt-3">
-            <BiMailSend /> : www.help@ecommerceapp.com
+            <BiMailSend /> : suleymanaslan.8006@gmail.com
           </p>
           <p className="mt-3">
             <BiPhoneCall /> : 012-3456789
           </p>
           <p className="mt-3">
-            <BiSupport /> : 1800-0000-0000 (toll free)
+            <BiSupport /> : 0000-0000-0000 (toll free)
           </p>
         </div>
       </div>
